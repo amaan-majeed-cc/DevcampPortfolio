51378e7cee773641d1fd33bb45bbc79aebfe45d9
Amaan Majeed <amaan.majeed@codingcops.com>
2024-10-08 15:50:10 +0500

    Implemented Destroy functionality for Portfolio Items

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -41,4 +41,14 @@ class PortfoliosController < ApplicationController
     @portfolio_item = Portfolio.find(params[:id])
   end
 
+  def destroy
+    @portfolio_item = Portfolio.find(params[:id])
+    
+    @portfolio_item.destroy
+    respond_to do |format|
+      format.html { redirect_to portfolios_path, notice: "Post was removed." }
+
+    end
+  end
+
 end
