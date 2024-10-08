df340e359e9eb25d2b9fb82f64493c45f6c7d13f
Amaan Majeed <amaan.majeed@codingcops.com>
2024-10-08 15:58:25 +0500

    Custom routes for Home

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
   resources :portfolios
-  get "pages/home"
-  get "pages/about"
-  get "pages/contact"
+  
+  root to: "pages#home"
+  get "about", to: "pages#about"
+  get "contact", to: "pages#contact"
+
   resources :blogs
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
