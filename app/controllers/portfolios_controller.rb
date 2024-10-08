35c32cf5e28a42216d4b464ec5523b0ba534e403
Amaan Majeed <amaan.majeed@codingcops.com>
2024-10-08 15:05:36 +0500

    Added new and Edit feature for Portfolio items

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -2,4 +2,39 @@ class PortfoliosController < ApplicationController
   def index
     @portfolio_items = Portfolio.all
   end
+
+  def new
+      @portfolio_item = Portfolio.new
+  end
+
+  # POST /portfolios or /portfolios.json
+  def create
+    @portfolio_item = Portfolio.new(params.require(:portfolio).permit(:title, :subtitle , :body))
+
+    respond_to do |format|
+      if @portfolio_item.save
+        format.html { redirect_to portfolios_path, notice: "Portfolio item was successfully created." }
+      else
+        format.html { render :new, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def edit
+    @portfolio_item = Portfolio.find(params[:id])
+  end
+
+  # PATCH/PUT /blogs/1 or /blogs/1.json
+  def update
+    @portfolio_item = Portfolio.find(params[:id])
+
+    respond_to do |format|
+      if @portfolio_item.update(params.require(:portfolio).permit(:title, :subtitle , :body))
+        format.html { redirect_to portfolios_path, notice: "Portfolio item was successfully updated." }
+      else
+        format.html { render :edit, status: :unprocessable_entity }
+      end
+    end
+  end
+
 end
