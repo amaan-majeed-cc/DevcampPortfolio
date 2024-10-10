9fbf43d512ac0de9f1be27a9fb6fea6001dc3b16
Amaan Majeed <amaan.majeed@codingcops.com>
2024-10-10 13:55:35 +0500

    Implemented View Helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+
+  def login_helper
+    if current_user
+      button_to "Logout", destroy_user_session_path, method: :delete, class: "btn btn-primary my-2 my-sm-0"
+    else
+      links =  link_to "Login", new_user_session_path, class: "btn btn-primary m-2 my-sm-0"
+      links += link_to "Register", new_user_registration_path, class: "btn btn-primary m-2 my-sm-0"
+      links.html_safe
+    end
+  end
 end
