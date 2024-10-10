fe51b70a1068d5e60ef411b3d92846c649584c10
Amaan Majeed <amaan.majeed@codingcops.com>
2024-10-10 13:01:57 +0500

    Implemented CSS on the Website

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,14 +1,15 @@
 class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
-
   before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :set_source
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
-    devise_parameter_sanitizer.permit(:account_update, keys: [:name])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [ :name ])
+    devise_parameter_sanitizer.permit(:account_update, keys: [ :name ])
   end
 
-  validates_presence_of :name
-
+  def set_source
+    session[:source] = params[:q] if params[:q]
+  end
 end
